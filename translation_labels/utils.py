26d092221f895a02a559344a7b9e29d59a34e2be
Svetlana Schmidt <sve13997>
2024-03-23 20:14:58 +0100

    add data reading

--- translation_labels/utils.py
+++ translation_labels/utils.py
@@ -2,6 +2,15 @@ import json
 import pandas as pd
 
 
+def create_column(source, target):
+    # define a list for prompts
+    src_tar=[]
+    # loop through zipped source and target segments
+    for src,trg in zip(source,target):
+            src_tar.append((src,trg))
+    return src_tar
+    
+
 def read_data(path):
     tq_data = pd.read_excel(path, header=0, usecols= [9,10,11,12,13,14,15,16,17,18,19,20,21])
     source_sentences = []
@@ -14,20 +23,22 @@ def read_data(path):
     target_rus = tq_data['eng-rus [flores200-eng_Latn-rus_Cyrl-devtest]']
 
 
-    # define a list for prompts
-    eng_deu=[]
-    # loop through zipped source and target segments
-    for src,trg in zip(source_eng,target_deu):
-            eng_deu.append((src,trg))
-    # assign list as new column in DataFrame
-    tq_data['eng-deu-pairs'] = eng_deu
-        # for line in open(path, mode='r', encoding='utf-8'):
-        #     reader = json.loads(line)
-        #     #line_lst = []
-        #     for k in reader.keys():
-        #         if k == 'sentence1':
-        #             source_sentences.append(reader[k])
-        #             target_sentences.append(reader[k])                 
 
-    return source_sentences
+    tq_data['eng-deu-pairs'] = create_column(source_eng, target_deu)
+    tq_data['eng-spa-pairs'] = create_column(source_eng, target_deu)
+    tq_data['eng-ben-pairs'] = create_column(source_eng, target_deu)
+    tq_data['eng-rus-pairs'] = create_column(source_eng, target_deu)
+    
+    eng_deu_pairs = create_column(source_eng, target_deu)
+    eng_spa_pairs = create_column(source_eng, target_spa)
+    eng_ben_pairs = create_column(source_eng, target_ben)
+    eng_rus_pairs = create_column(source_eng, target_rus)
+  
+            
+    return {
+    'eng_deu_pairs': eng_deu_pairs,
+    'eng_spa_pairs': eng_spa_pairs,
+    'eng_ben_pairs': eng_ben_pairs,
+    'eng_rus_pairs': eng_rus_pairs
+    }
 
